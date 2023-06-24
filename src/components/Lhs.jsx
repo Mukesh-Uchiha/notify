5049c4c3c2521e28548130cba49b7de086a85757
Mukesh N <mukesh.it20@bitsathy.ac.in>
2023-06-24 11:33:52 +0530

    from changes

--- src/components/Lhs.jsx
+++ src/components/Lhs.jsx
@@ -1,35 +1,15 @@
 import React, { useState } from 'react';
-import {
-  Avatar,
-  ChakraProvider,
-  Circle,
-  FormControl,
-  FormLabel,
-  Input,
-  Tooltip,
-  Switch,
-} from '@chakra-ui/react';
+import { Avatar, ChakraProvider, Circle, Tooltip } from '@chakra-ui/react';
 
 import { Button, ButtonGroup } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 
-function Lhs() {
-  const formDetail = [
-    { placeholder: 'company name', label: 'company ' },
-    { placeholder: 'Enter the role(s)', label: ' role' },
-    { placeholder: 'Enter the CTC ', label: 'Salary' },
-    { placeholder: 'Enter the Location', label: 'Company Location' },
-    {
-      placeholder: 'Enter the last Date ',
-      label: 'Deadline',
-      type: 'datetime-local',
-    },
-  ];
-
-  const [Status, ChangeStatus] = useState(false);
+function Lhs({ onButtonClicked }) {
+  const [Status, ChangeStatus] = useState(true);
   const changeFormMenu = () => {
     ChangeStatus(!Status);
     console.log(Status);
+    onButtonClicked(Status);
   };
 
   const [signUp, ChangeSignUpStatus] = useState(true);
@@ -38,29 +18,11 @@ function Lhs() {
     console.log(signUp);
   };
 
-  const [eligibility, ChangeEligibility] = useState(false);
-  const [throughout, ChangeThroughout] = useState(false);
-  const [Bound, ChangeBound] = useState(false);
-
   const getScroll = () => {
     console.log(window.scrollY);
   };
-  window.addEventListener("scroll", getScroll);
+  window.addEventListener('scroll', getScroll);
 
-  function handleCheck(event) {
-    const name = event.target.id;
-    if (name === 'eligibility') {
-      return ChangeEligibility(!eligibility);
-    } else if (name === 'throughout') {
-      return ChangeThroughout(!throughout);
-    } else if (name === 'Bound') {
-      return ChangeBound(!Bound);
-    }
-  }
-<<<<<<< HEAD
-
-=======
->>>>>>> ea3f5a90dfd8899e2b725172f5c2a759abefd0ca
   return (
     <>
       <div className="lhs">
@@ -72,9 +34,9 @@ function Lhs() {
             className="OpenMenuButton"
             onClick={changeFormMenu}
             style={
-              Status
+              !Status
                 ? {
-                    transform: 'rotate(315deg)',
+                    transform: 'rotate(315deg) ',
                   }
                 : {
                     transform: 'rotate(0deg)',
@@ -104,180 +66,7 @@ function Lhs() {
           </ButtonGroup>
         </ChakraProvider>
       </div>
-      <div
-        className="Form"
-        style={
-          Status
-<<<<<<< HEAD
-            ? { top: "0px", transform: "rotateX(0deg)" }
-            : { top: "-100%", transform: "rotateX(90deg)" }
-=======
-            ? { top: '50px', transform: 'rotateX(0deg)' }
-            : { top: '-60%', transform: 'rotateX(90deg)' }
->>>>>>> ea3f5a90dfd8899e2b725172f5c2a759abefd0ca
-        }
-      >
-        <div>
-          <h1
-            className="Formh1"
-            style={
-              Status
-                ? { transform: 'translate(10px, 0px)' }
-                : { transform: 'translate(10px, -50px)' }
-            }
-          >
-            Fill Details
-          </h1>
-        </div>
-        <form>
-          <ChakraProvider>
-            <div className="inputContainer">
-              {formDetail.map((formDetail, index) => (
-                <div key={index}>
-                  <FormControl isRequired>
-                    <FormLabel className="Labels">
-                      {' '}
-                      {formDetail.label}
-                    </FormLabel>
-                    <Input
-                      color="white"
-                      placeholder={formDetail.placeholder}
-                      type={formDetail.type}
-                      size="sm"
-                    />
-                  </FormControl>
-                </div>
-              ))}
-              <div>
-                <FormControl isRequired>
-                  <FormLabel className="Labels">Internship</FormLabel>
-                  <Input
-                    color="white"
-                    placeholder="Internship period"
-                    size="sm"
-                  />
-                </FormControl>
-              </div>
-              <div>
-                <FormLabel className="Labels" mt="1">
-                  Eligibilty🎓
-                </FormLabel>
-                <FormControl
-                  isRequired
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <FormLabel fontWeight="normal" mb="0" color="#C9C9C9">
-                    If There is eligibility criteria
-                  </FormLabel>
-                  <Switch id="eligibility" isRequred onChange={handleCheck} />
-                </FormControl>
-                {eligibility && (
-                  <div>
-                    <FormControl
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="space-between"
-                      mt={2}
-                      mb={2}
-                    >
-                      <FormLabel fontWeight="normal" ml={0} color="#C9C9C9">
-                        If it not is through out{' '}
-                      </FormLabel>
-                      <Switch
-                        id="throughout"
-                        isRequred
-                        onChange={handleCheck}
-                      />
-                    </FormControl>
-                    {throughout ? (
-                      <FormControl
-                        display="flex"
-                        justifyContent="space-between"
-                      >
-                        <Input
-                          color="white"
-                          placeholder="10th"
-                          size="sm"
-                          // width="70px"
-                          m={2}
-                        />
-                        <Input
-                          color="white"
-                          placeholder="Dip/12th"
-                          size="sm"
-                          // width="80px"
-                          m={2}
-                        />
-                        <Input
-                          color="white"
-                          placeholder="UG"
-                          size="sm"
-                          // width="70px"
-                          m={2}
-                        />
-                      </FormControl>
-                    ) : (
-                      <>
-                        <Input
-                          color="white"
-                          placeholder="If it is through out"
-                          size="sm"
-                          m={2}
-                          ml={0}
-                        />
-                      </>
-                    )}
-                  </div>
-                )}
-              </div>
-              <div>
-                <FormLabel className="Labels">Bound</FormLabel>
-                <FormControl
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <FormLabel fontWeight="normal" mb="0" color="#C9C9C9">
-                    If There is no Bound
-                  </FormLabel>
-                  <Switch id="Bound" isRequred onChange={handleCheck} />
-                </FormControl>
-                {Bound && (
-                  <Input color="white" placeholder="Bound period" size="sm" />
-                )}
-              </div>
-            </div>
-          </ChakraProvider>
-        </form>
-        <div className="ButtonContainer">
-          <ChakraProvider>
-            <ButtonGroup spacing="6">
-              <Button
-                colorScheme="blue"
-                style={
-                  Status
-                    ? { transform: 'translate(0px, 0px)' }
-                    : { transform: 'translate(0px, 50px)' }
-                }
-              >
-                Save
-              </Button>
-              <Button
-                colorScheme="gray"
-                style={
-                  Status
-                    ? { transform: 'translate(0px, 0px)' }
-                    : { transform: 'translate(0px, 50px)' }
-                }
-              >
-                Cancel
-              </Button>
-            </ButtonGroup>
-          </ChakraProvider>
-        </div>
-      </div>
+
       <div className="FakeMenu"></div>
     </>
   );
